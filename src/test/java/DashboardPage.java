b3b6f3061a1da2dd17cb1d10e0b1040dccac917a
Md. Nazmus Shakib <robin.labib@gmail.com>
2023-01-14 22:27:56 +0600

    Removed old code

--- src/test/java/DashboardPage.java
+++
@@ -1,8 +0,0 @@
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class DashboardPage {
-    @FindBy(xpath = "//span[contains(text(),'PROCUREMENT MANAGEMENT')]")
-    WebElement menuProc;
-
-}
